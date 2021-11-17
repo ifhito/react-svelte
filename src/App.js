1aef6bfc4b125b6876d8de1d9c74c2c12e605f29
ifhito <hito01010101@gmail.com>
2021-11-17 21:32:50 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import test from './svelte/test.svelte';
+import { useEffect, useRef } from 'react';
+import toReact from "svelte-adapter/react";
 function App() {
+  const SvelteInReact = toReact(test,{},'div');
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <SvelteInReact />
         </p>
         <a
           className="App-link"
@@ -15,9 +19,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
+      
     </div>
   );
 }
